b66ccae2d79280b69edd0262fee1174a44b3d239
rriarrirra <d.satira04@gmail.com>
2026-04-18 22:19:59 +0700

    schema.sql

--- modules/modul_7/config/schema.sql
+++ modules/modul_7/config/schema.sql
@@ -5,11 +5,21 @@ SET time_zone = "+00:00";
 CREATE TABLE `screening_results` (
   `id` int(11) NOT NULL,
   `patient_id` int(11) NOT NULL,
+
+  `full_name` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  `skin_type` varchar(100) NOT NULL,
+  `main_concern` text NOT NULL,
+  
   `image_path` varchar(255) NOT NULL,
-  `ml_severity_level` enum('Mild','Moderate','Severe') NOT NULL,
+  
+  -- Update Enum agar sinkron dengan hasil deteksi Teachable Machine
+  `ml_severity_level` enum('Mild','Moderate','Severe','PAPULE','PUSTULE','BLACKHEAD') NOT NULL,
+  
   `ml_papule_count` int(11) DEFAULT 0,
   `ml_pustule_count` int(11) DEFAULT 0,
   `ml_blackhead_count` int(11) DEFAULT 0,
+  
   `status` enum('completed_by_ml','pending_doctor_review','reviewed_by_doctor') NOT NULL DEFAULT 'completed_by_ml',
   `doctor_id` int(11) DEFAULT NULL,
   `doctor_notes` text DEFAULT NULL,
@@ -20,5 +30,5 @@ ALTER TABLE `screening_results`
   ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `screening_results`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
